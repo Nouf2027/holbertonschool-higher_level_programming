e6688978071d46cf0f8abc40f9a98b5fd0d1f90a
root <root@ip-10-42-38-195.ec2.internal>
2025-11-29 18:11:47 +0000

    A

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+a = 10
+b = 5
+
+import calculator_1
+
+if name == "__main__":
+    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
