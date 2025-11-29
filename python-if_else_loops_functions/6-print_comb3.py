a54261cabdd9b2ff9b473d03299b37c5c3bbe770
root <root@ip-10-42-38-195.ec2.internal>
2025-11-29 15:30:24 +0000

    A

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i != 99:
-        print("{:02d}, ".format(i), end="")
-    else:
-        print("{:02d}".format(i))
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print("{:d}{:d}, ".format(i, j), end="")
+        else:
+            print("{:d}{:d}".format(i, j))
