ff468c974bae36009b1bcbae281e562a6353884e
root <root@ip-10-42-38-195.ec2.internal>
2025-11-29 18:14:46 +0000

    fix main condition

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ b = 5
 
 import calculator_1
 
-if name == "__main__":
+if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
     print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
     print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
