cc3cc897f211e1c5c64d1684202a717dda34c6c3
root <root@ip-10-42-177-93.ec2.internal>
2025-11-29 18:46:15 +0000

    a

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     total = 0
     for i in range(1, len(sys.argv)):
         total += int(sys.argv[i])
-
     print(total)
+
