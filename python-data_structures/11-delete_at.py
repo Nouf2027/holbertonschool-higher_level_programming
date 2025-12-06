db3a1204f80e56f03aad9d01d9ff962dc80f5a9c
root <root@ip-10-42-232-163.ec2.internal>
2025-12-06 16:39:13 +0000

    A

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-
-    # create a new list without the element at idx
-    new_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
-
-    return new_list
+    return my_list[:idx] + my_list[idx+1:]
