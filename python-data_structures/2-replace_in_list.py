b80d04183d733da1f960fb4574962eeaaed5eaa9
root <root@ip-10-42-195-194.ec2.internal>
2025-12-06 11:37:32 +0000

    A

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specific position."""
     if idx < 0 or idx >= len(my_list):
+
         return my_list
 
-    new_list = my_list.copy()
-    new_list[idx] = element
-    return new_list
+    my_list[idx] = element
+    return my_list
