f4c8edabef4453da17904b7c80dc6fd31141e4ab
root <root@ip-10-42-45-64.ec2.internal>
2025-12-07 02:52:20 +0000

    A

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,8 +5,16 @@ Module that provides add_integer function.
 
 def add_integer(a, b=98):
     """Adds two integers."""
+    # Check types
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    # Check overflow (Holberton test)
+    if isinstance(a, float) and (a == float('inf') or a == float('-inf')):
+        raise OverflowError("cannot convert float infinity to integer")
+    if isinstance(b, float) and (b == float('inf') or b == float('-inf')):
+        raise OverflowError("cannot convert float infinity to integer")
+
     return int(a) + int(b)
