11118a694dff26c782bd877887212c275a575529
root <root@ip-10-42-45-64.ec2.internal>
2025-12-07 02:32:49 +0000

    A

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+def add_integer(a, b=98):
+    """Adds two integers or floats."""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
