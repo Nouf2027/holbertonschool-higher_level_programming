3f5cbc9fd14cd80b766961183abd9578b5a15b28
root <root@ip-10-42-45-64.ec2.internal>
2025-12-07 05:05:27 +0000

    a

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,8 +4,8 @@
 
 def matrix_divided(matrix, div):
     """Divide all elements of a matrix by div."""
-
     msg = "matrix must be a matrix (list of lists) of integers/floats"
+
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
             not all(isinstance(num, (int, float))
@@ -21,10 +21,5 @@ def matrix_divided(matrix, div):
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    ·
-    if div == float("inf") or div == float("-inf"):
-        return [[0.0 for _ in row] for row in matrix]
-
 
     return [[round(num / div, 2) for num in row] for row in matrix]
