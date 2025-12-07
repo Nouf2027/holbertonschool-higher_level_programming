e6cec4a35cf708aaf865b679b5f7273455fe3bd8
root <root@ip-10-42-45-64.ec2.internal>
2025-12-07 04:56:43 +0000

    a

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,16 +3,18 @@
 
 
 def matrix_divided(matrix, div):
-    """Divide all elements of a matrix."""
-    if (not isinstance(matrix, list) or
+    """
+    Divide all elements of a matrix.
+    """
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
             not all(isinstance(num, (int, float))
                     for row in matrix for num in row)):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
+        raise TypeError(msg)
 
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+    row_len = len(matrix[0])
+    if not all(len(row) == row_len for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
@@ -21,17 +23,4 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for num in row:
-            result = num / div
-
-            # handle infinity and NaN (hidden test)
-            if result == float("inf") or result == float("-inf") or result != result:
-                result = 0.0
-
-            new_row.append(round(result, 2))
-        new_matrix.append(new_row)
-
-    return new_matrix
+    return [[round(num / div, 2) for num in row] for row in matrix]
