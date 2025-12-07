b0ee6b936a1477b611f7ada274990ba33ccf1bdc
root <root@ip-10-42-45-64.ec2.internal>
2025-12-07 02:50:23 +0000

    a

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Adds two integers or floats.
+Module that provides add_integer function.
 """
 
 def add_integer(a, b=98):
+    """Adds two integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
