a0ddb8a1e25a8c7c819a39455237d72c8f525972
root <root@ip-10-42-185-108.ec2.internal>
2025-12-08 09:54:50 +0000

    a

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
+"""A function that prints a text with 2 new lines after '.', '?' and ':'"""
+
 def text_indentation(text):
-    """Prints text with 2 new lines after '.', '?' and ':'"""
-    if type(text) is not str:
+    """Prints text with two new lines after '.', '?', ':'"""
+
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
 
     result = ""
-    for char in text:
+    i = 0
+
+    while i < len(text):
+        char = text[i]
         result += char
+
         if char in ".?:":
             result += "\n\n"
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
 
-    lines = result.split("\n")
-    for line in lines:
-        print(line.strip(), end="")
-        if line != lines[-1]:
-            print()
+        i += 1
+
+    print(result, end="")
