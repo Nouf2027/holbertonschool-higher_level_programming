224f2725d7da4e69af46d511cf3a434e7580f33f
root <root@ip-10-42-98-194.ec2.internal>
2025-12-13 05:06:37 +0000

    a

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Defines a square"""
+"""Defines a Square class."""
+
 
 class Square:
-    """Square class with private size attribute"""
+    """Represents a square."""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
+        """Initialize a new Square with a private size attribute."""
         self.__size = size
