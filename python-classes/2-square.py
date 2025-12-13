e91e6ca6d2328b8958736eca241b32fdb42cea85
root <root@ip-10-42-98-194.ec2.internal>
2025-12-13 05:09:44 +0000

    a

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+"""Defines a Square class with size validation."""
 
 
 class Square:
     """Represents a square."""
 
-    def __init__(self, size):
-        """Initialize a new Square with a private size."""
+    def __init__(self, size=0):
+        """Initialize a new Square with a validated private size."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
         self.__size = size
