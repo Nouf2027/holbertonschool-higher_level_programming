358574325dfd13d10238f764ab1745e0f41db8ba
root <root@ip-10-42-180-220.ec2.internal>
2025-12-20 13:52:30 +0000

    a

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,5 +2,5 @@
 """Defines a lookup function"""
 
 def lookup(obj):
-    """Return the list of available attributes and  methods of an object"""
-      return dir(obj)
+    """Return the list of available attributes and methods of an object"""
+    return dir(obj)
